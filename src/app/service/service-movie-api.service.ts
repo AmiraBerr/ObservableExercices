12a20411b6b87a68a89091626ce450e42a51c3a9
mac <mac@MacBook-Air.local>
2020-01-30 20:35:16 +0100

    commit

--- src/app/service/service-movie-api.service.ts
+++ src/app/service/service-movie-api.service.ts
@@ -7,15 +7,15 @@ import { Observable } from 'rxjs';
 export class ServiceMovieApiService {
   constructor(private $http: HttpClient) {}
   getMovie(): Observable<any> {
-    return this.$http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=37b20774c900317af3cf8f43c7bb3f4f&page=1');
+    return this.$http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=5b5056162db4012d0c06720c61d4c1db&page=1');
 }
 
   getImages(id){
-  return this.$http.get('https://api.themoviedb.org/3/movie/'+id+'/images?api_key=37b20774c900317af3cf8f43c7bb3f4f');
+  return this.$http.get('https://api.themoviedb.org/3/movie/'+id+'/images?api_key=5b5056162db4012d0c06720c61d4c1db');
 }
 
  getVideos(id){
-  return this.$http.get('https://api.themoviedb.org/3/movie/'+id+'/videos?api_key=37b20774c900317af3cf8f43c7bb3f4f');
+  return this.$http.get('https://api.themoviedb.org/3/movie/'+id+'/videos?api_key=5b5056162db4012d0c06720c61d4c1db');
 }
 
 
